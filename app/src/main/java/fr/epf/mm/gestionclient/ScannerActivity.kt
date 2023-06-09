95f27ae31726615a79e38c90ce61448d0d62890c
leo <leocomte6@gmail.com>
2023-06-09 09:18:37 +0200

    mise en commun

--- app/src/main/java/fr/epf/mm/gestionclient/ScannerActivity.kt
+++ app/src/main/java/fr/epf/mm/gestionclient/ScannerActivity.kt
@@ -1,28 +1,103 @@
 package fr.epf.mm.gestionclient
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.SeekBar
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.ViewGroup
+import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+import com.google.zxing.Result
 
 
-private const val TAG = "AddClientActivity"
 
-class ScannerActivity: AppCompatActivity() {
+//private const val TAG = "AddClientActivity"
+
+class ScannerActivity: AppCompatActivity(), SurfaceHolder.Callback, ZXingScannerView.ResultHandler {
+    private lateinit var scannerView: ZXingScannerView
+    private lateinit var surfaceView: SurfaceView
+
+    private val CAMERA_PERMISSION_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sanner)
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraIds = cameraManager.cameraIdList
+        val numberOfCameras = cameraIds.size
+        Log.d("cam", "${numberOfCameras}")
+        surfaceView = findViewById(R.id.surface_view)
+        scannerView = ZXingScannerView(this)
+        scannerView.setAutoFocus(true)
+        scannerView.setFormats(listOf(BarcodeFormat.QR_CODE))
+        surfaceView.holder.addCallback(this)
+    }
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
+            startCamera()
+        } else {
+            startCamera()
+        }
+    }
 
-        Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            }
+        }
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        scannerView.startCamera()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        scannerView.stopCamera()
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        // Le résultat du scan de code QR est disponible ici
+        val resultText : String? = rawResult?.text
+        // Faites quelque chose avec le résultat (par exemple, affichage dans un TextView)
+        // textView.text = resultText
+        // Redémarrez le scanner pour continuer à scanner d'autres codes
+        scannerView.resumeCameraPreview(this)
+    }
+
+    private fun startCamera() {
+        scannerView.setResultHandler(this)
+        scannerView.startCamera()
+        val holder = surfaceView.holder
+        holder.addCallback(this)
+    }
+
+}
+       /* Log.d(TAG, "onCreate() called with: savedInstanceState = $savedInstanceState")
         val addButton = findViewById<Button>(R.id.add_client_add_button)
 
 
@@ -50,8 +125,5 @@ class ScannerActivity: AppCompatActivity() {
 
 
             finish()
-        }
+        }*/
 
-
-    }
-}
